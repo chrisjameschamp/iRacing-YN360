12f96158bfe91fab07369b5f20f6c5a625f5d3f2
Chris Champeau <semaj4712@gmail.com>
2023-02-01 14:15:21 -0800

    V0.1.1: Initial Build of Project

---
+++ util/constants.py
@@ -0,0 +1,226 @@
+import asyncio
+import bleak
+import irsdk
+import pytimedinput
+import re
+import uuid
+
+from util import functions
+
+VERSION = '0.1.1'
+
+class ir_yn360:
+    def __init__(self):
+        self.ir = irsdk.IRSDK()
+        self.ir_connected = False
+        self.current_color = [0,0,0] #RGB 0-255
+        self.rest_color = [0,0,255]
+        self.flags = {'bit': 0, 'active': {}, 'cycle': 1, 'flashing': '', 'waiving': ''}
+        self.flash_rate = 3
+
+    async def start(self):
+        print('Welcome to iRacing YN360\n')
+        functions.checkVersion(VERSION)
+        address = self.getAddress()
+        device = functions.getDevice()
+        if device:
+            user_input, timed_out = pytimedinput.timedKey(f'Existing Device Found - {device[1]}\nPress any key to select a new device or wait 5 seconds...')
+            if not timed_out:
+                print('')
+                device = await self.scan()
+        else:
+            device = await self.scan()
+
+        await self.run(device, address)
+
+    
+    async def run(self, device, address):
+        client = bleak.BleakClient(device[1])
+        try:
+            await client.connect()
+            while True:
+                if self.checkiRacing():
+
+                    # Flags
+                    diff = self.flags['bit'] ^ self.ir['SessionFlags']
+                    if diff:
+                        self.flags['bit'] = self.ir['SessionFlags']
+
+                        for flag in functions.FLAGS:
+                            if diff & flag[0]:
+                                if flag[0] & self.flags['bit']:
+                                    if flag[1] not in self.flags['active'].keys():
+                                        self.flags['active'][flag[1]] = flag[0]
+                                else:
+                                    if flag[1] in self.flags['active'].keys():
+                                        del self.flags['active'][flag[1]]
+
+                    if 'checkered' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'checkered':
+                            print('Checkered Flag Waiving')
+                            self.flags['waiving'] != 'checkered'
+                        if self.flags['flashing'] != 'checkered':
+                            self.flags['cycle'] = self.flash_rate
+                            self.flags['flashing'] = 'checkered'
+                        if self.flags['cycle'] % self.flash_rate == 0:
+                            if self.current_color != [255,255,255]:
+                                self.setColor([255,255,255], client, address)
+                            else:
+                                self.setColor([0,0,0], client, address)
+                    elif 'caution_waving' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'caution':
+                            print('Caution Flag Waiving')
+                            self.flags['waiving'] != 'caution'
+                        self.flags['flashing'] = ''
+                        self.setColor([255,255,0], client, address)
+                    elif 'yellow_waving' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'yellow':
+                            print('Yellow Flag Waiving')
+                            self.flags['waiving'] != 'yellow'
+                        self.flags['flashing'] = ''
+                        self.setColor([255,255,0], client, address)
+                    elif 'start_go' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'green':
+                            print('Green Flag Waiving')
+                            self.flags['waiving'] != 'green'
+                        self.flags['flashing'] = ''
+                        self.setColor([0,255,0], client, address)
+                    elif 'repair' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'repair':
+                            print('Meatball Flag Waiving')
+                            self.flags['waiving'] != 'repair'
+                        if self.flags['flashing'] != 'repair':
+                            self.flags['cycle'] = self.flash_rate
+                            self.flags['flashing'] = 'repair'
+                        if self.flags['cycle'] % self.flash_rate == 0:
+                            if self.current_color != [255,140,0]:
+                                self.setColor([255,140,0], client, address)
+                            else:
+                                self.setColor([0,0,0], client, address)
+                    elif 'white' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'white':
+                            print('White Flag Waiving')
+                            self.flags['waiving'] != 'white'
+                        self.flags['flashing'] = ''
+                        self.setColor([255,255,255], client, address)
+                        self.flags['flashing'] = ''
+                    elif 'debris' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'yellow':
+                            print('Yellow Flag Waiving')
+                            self.flags['waiving'] != 'yellow'
+                        self.flags['flashing'] = ''
+                        self.setColor([255,255,0], client, address)
+                    elif 'green' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'green':
+                            print('Green Flag Waiving')
+                            self.flags['waiving'] != 'green'
+                        self.flags['flashing'] = ''
+                        self.setColor([0,255,0], client, address)
+                    elif 'red' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'red':
+                            print('Red Flag Waiving')
+                            self.flags['waiving'] != 'red'
+                        self.flags['flashing'] = ''
+                        self.setColor([255,0,0], client, address)
+                    elif 'caution' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'caution':
+                            print('Caution Flag Waiving')
+                            self.flags['waiving'] != 'caution'
+                        self.flags['flashing'] = ''
+                        self.setColor([255,255,0], client, address)
+                    elif 'yellow' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'yellow':
+                            print('Yellow Flag Waiving')
+                            self.flags['waiving'] != 'yellow'
+                        self.flags['flashing'] = ''
+                        self.setColor([255,255,0], client, address)
+                    elif 'blue' in self.flags['active'].keys():
+                        if self.flags['waiving'] != 'blue':
+                            print('Blue Flag Waiving')
+                            self.flags['waiving'] != 'blue'
+                        self.flags['flashing'] = ''
+                        self.setColor([0,0,255], client, address)
+                    else:
+                        self.flags['waiving'] = ''
+                            
+                    self.flags['cycle'] += 1
+                    await asyncio.sleep(1)
+                else:
+                    self.setColor(self.rest_color, client, address)
+                    await asyncio.sleep(3)
+        except Exception as e:
+            print(f'Error: {e}')
+        finally:
+            await client.disconnect()
+
+
+    async def scan(self):
+        print('Scanning for BLE Devices...\n')
+        try:
+            devices = await bleak.BleakScanner.discover()
+        except Exception as e:
+            print(f'Error: {e}')
+            function.closeApp()
+        if not devices:
+            print('No BLE devices found')
+            function.closeApp()
+        names = []
+        ## Probably could just find the device that is YN360
+        for i, item in enumerate(devices):
+            device = str(item).split(':')
+            name = device[-1].lstrip()
+            device = device[:-1]
+            device = ':'.join(device)
+            if name != 'None':
+                names.append([name, device])
+        print(f'Found {len(names)}')
+        for i, item in enumerate(names, start=1):
+            print(f'  {i}) {item[0]}')
+        while True:
+            print('\nSelect the corresponding number next to the YN360.  All other devices may not work.')
+            user_input = input('>>> ')
+            try:
+                value = int(user_input)
+                if value >= 1 and value <= len(names):
+                    device = names[int(user_input)-1]
+                    print(f'You chose: {device[0]}')
+                    print('Confirm Y/N?')
+                    if input('>>> ').lower() == 'y':
+                        break
+                else:
+                    print(f'Invalid input, please enter a number between 1 and {len(names)}')
+            except ValueError:
+                print(f'Invalid input, please enter a number between 1 and {len(names)}')
+        functions.saveDevice(device)
+        return device
+
+
+    def getAddress(self):
+        address = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+        print(f'System Address: {address}\n')
+        return address
+
+
+    async def setColor(self, color, client, address):
+        if color != self.current_color:
+            await client.write_gatt_char(address, bytearray([0xae,0xa1,color[0],color[1],color[2]]), response=True)
+            self.current_color = color
+
+    def checkiRacing(self):
+        if self.ir_connected and not (self.ir.is_initialized and self.ir.is_connected):
+            self.ir_connected = False
+            self.ir.shutdown()
+            print('iRacing Disconnected')
+            return False
+        elif not self.ir_connected and self.ir.startup() and self.ir.is_initialized and self.ir.is_connected:
+            self.ir_connected = True
+            print('iRacing Connected')
+            return True
+        elif self.ir_connected:
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    iryn = ir_yn360()
+    asyncio.run(iryn.start())
---
+++ util/functions.py
@@ -0,0 +1,109 @@
+import json
+import os
+import requests
+import sys
+
+from packaging.version import Version
+from appdirs import *
+
+APPDATA_FOLDER = user_data_dir('iRacing-YN360', 'ChrisJamesChamp')
+
+def ensureAppDataFolder():
+    folder = APPDATA_FOLDER
+    #print('Ensuring App Data Folder Exists')
+    if not os.path.exists(folder):
+        print('App Data Folder Does Not Exist')
+        print('Creating App Data Folder...')
+
+        try:
+            os.makedirs(folder)
+            print('Created App Data Folder\n')
+        except:
+            print('Could not open the App Data Folder\n')
+            closeApp()
+
+    else:
+        #print(f'App Data Folder Exists: {folder}')
+        pass
+
+def closeApp():
+    print('Exiting...')
+    sys.exit();
+
+def getDevice():
+    ensureAppDataFolder()
+    try:
+        with open(f'{APPDATA_FOLDER}/device.json') as infile:
+            data = json.load(infile)
+        print('Loaded Existing Device\n')
+        return data
+    except:
+        print('No Existing Device Found\n')
+        return None
+
+def saveDevice(device):
+    ensureAppDataFolder()
+    print('Saving Device...')
+    try:
+        with open(f'{APPDATA_FOLDER}/device.json', 'w') as outfile:
+            json.dump(device, outfile)
+        print('Device Saved')
+    except Exception as e:
+        print(f'Warning: Could Not Save Device. Error: {e}')
+
+def checkVersion(version):
+    print(f'Version: {version}')
+    print('Checking for Updates...')
+    
+    url = 'https://api.github.com/repos/chrisjameschamp/iRacing-YN360/releases/latest'
+    response = requests.get(url).json()
+    if 'tag_name' in response:
+        gitVersion = Version(response['tag_name'])
+        curVersion = Version(version)
+
+        if gitVersion > curVersion:
+            print(f'There is a new version of iRacing-YN360 available\nThe Most recent version is {gitVersion}')
+            print('Get the latest version by visiting https://github.com/chrisjameschamp/iRacing-YN360\n')
+        else:
+            print('You are up to date\n')
+
+    else:
+        print('Error Checking for Updates')
+
+FLAGS = [
+    ## Global Flags
+    (0x0001, 'checkered'),
+    (0x0002, 'white'),
+    (0x0004, 'green'),
+    (0x0008, 'yellow'),
+    (0x0010, 'red'),
+    (0x0020, 'blue'),
+    (0x0040, 'debris'),
+    (0x0080, 'crossed'),
+    (0x0100, 'yellow_waving'),
+    (0x0200, 'one_lap_to_green'),
+    (0x0400, 'green_held'),
+    (0x0800, 'ten_to_go'),
+    (0x1000, 'five_to_go'),
+    (0x2000, 'random_waving'),
+    (0x4000, 'caution'),
+    (0x8000, 'caution_waving'),
+
+    ## Drivers Black Flags
+    (0x010000, 'black'),
+    (0x020000, 'disqualified'),
+    (0x040000, 'serviceable'), # Car Is Allowed Service (Not A Flag)
+    (0x080000, 'furled'),
+    (0x100000, 'repair'),
+
+    ## Start Lights
+    (0x10000000, 'start_hidden'),
+    (0x20000000, 'start_ready'),
+    (0x40000000, 'start_set'),
+    (0x80000000, 'start_go'),
+
+    ## Pace Flags
+    (0x01, 'end_of_line'),
+    (0x02, 'free_pass'),
+    (0x04, 'waved_around')
+]
